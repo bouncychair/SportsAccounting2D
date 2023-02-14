f61bf1a55aec98fdc666f1fba0346c15da6e7029
Miroslav Penchev <miroslav.penchev@student.nhlstenden.com>
2023-02-14 14:37:47 +0100

    Code conventions & upload file functionality

--- PythonAPI/recieveRequest.py
+++ PythonAPI/recieveRequest.py
@@ -1,3 +1,4 @@
+import mt940
 from bson import json_util, ObjectId
 from flask import Flask, jsonify, request, make_response
 from pymongo import MongoClient
@@ -11,39 +12,36 @@ client = MongoClient(connection_string)
 db = client["ProjTest"]
 collection = db["MT940Parsed"]
 
-alternative = {'description': 'salary',
-               'amount': 5000}
-
 
 @app.route('/api/test', methods=["GET"])
 def test():
     return "Hi"
 
 
-@app.route('/api/upload', methods=["GET", "POST"])
-# IF NO FILE IS UPLOADED, IT WILL ADD THE "ALTERNATIVE" DICTIONARY TO THE COLLECTION. JUST FOR TESTING
+# IF FILE IS SEND FROM THE C# APP, IT WILL ADD THE FILE TO THE COLLECTION
+@app.route('/api/uploadFile', methods=["POST"])
 def save_file_to_collection():
-    if request.method == "GET":
-        collection.insert_one(alternative)
-
-    # THE INTENDED WAY: IF FILE IS SEND FROM THE C# APP, IT WILL ADD THE FILE TO THE COLLECTION
-    if request.method == "POST":
-        file = request.get_json()
-        file = json.dumps(file, default=json_util.default)
-        file = json.loads(file)
-        collection.insert_one(file)
+    file = request.files['file']
+    file = parse_mt940_file(file)
+    collection.insert_one(file)
     return "File ADDED successfully"
 
 
 # GETS ALL THE DOCUMENTS FROM THE COLLECTION
-@app.route('/api/getCollection', methods=["GET"])
-def get_collection():
+@app.route('/api/getTransactions', methods=["GET"])
+def get_transactions():
     return make_response(json.loads(json_util.dumps(collection.find())))
+
+
+# GETS THE DOCUMENT WITH THE GIVEN ID
+@app.route('/api/getTransaction/<transaction_id>', methods=["GET"])
+def get_transaction(transaction_id):
+    return make_response(json.loads(json_util.dumps(collection.find({"_id": ObjectId(transaction_id)}))))
 
 
 # GETS THE FINAL BALANCE OF THE DOCUMENT WITH THE GIVEN ID
 @app.route('/api/getBalance/<transaction_id>', methods=["GET"])
-def get_transactions(transaction_id):
+def get_balance(transaction_id):
     balance = collection.find({"_id": ObjectId(transaction_id)},
                               {"forward_available_balance": {"amount": 1}, "_id": 0})
     balance = json.loads(json_util.dumps(balance))
@@ -53,7 +51,7 @@ def get_transactions(transaction_id):
 # GETS THE DOCUMENT WITH THE GIVEN ID AND THE GIVEN ELEMENT AND POSITION IF THE ELEMENT IS "TRANSACTIONS" ELEMENT CAN
 # BE ANY MAIN ELEMENT IN THE DOCUMENT, LIKE "TRANSACTIONS", "FORWARD_AVAILABLE_BALANCE", "SEQUENCE_NUMBER", ETC.
 # EXAMPLE:http://127.0.0.1:122/api/getSomething?transaction_id=63e3aadd91fbc6374c13ef54&element=forward_available_balance
-@app.route('/api/searchForSomething', methods=["GET"])
+@app.route('/api/searchKeyword', methods=["GET"])
 def search():
     transaction_id = request.args.get('transaction_id')
     element_to_get = request.args.get('element')
@@ -66,6 +64,16 @@ def search():
                                   {element_to_get: 1, "_id": 0})
     balance = json.loads(json_util.dumps(balance))
     return make_response(balance)
+
+
+def parse_mt940_file(file_path):
+    # PARSES THE MT940 FILE AND RETURNS A COLLECTION
+    transaction = mt940.parse(file_path)
+    # CONVERTS THE COLLECTION TO JSON STRING
+    transaction = json.dumps(transaction, indent=4, sort_keys=True, cls=mt940.JSONEncoder)
+    # CONVERTS THE JSON STRING TO A DICTIONARY
+    transaction = json.loads(transaction)
+    return transaction
 
 
 if __name__ == '__main__':
--- SendRequests/SendRequests/Program.cs
+++ SendRequests/SendRequests/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,54 +14,30 @@ namespace SendRequests
 {
     internal class Program
     {
-
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             //MONGODB CONNECTION CAUSE WHY NOT
-            /* string connectionString = "mongodb+srv://adman:qweasd123@projectdb.ud8fb4w.mongodb.net/?retryWrites=true&w=majority";
-
+            /*
              MongoClient dbClient = new MongoClient(connectionString);
 
              var database = dbClient.GetDatabase("ProjTest");
              var collection = database.GetCollection<BsonDocument>("MT940Parsed");
 
              var firstDocument = collection.Find(new BsonDocument()).FirstOrDefault();
-             //Console.WriteLine(firstDocument.ToString());*/
+             Console.WriteLine(firstDocument.ToString());
+            */
 
-            /*string content = File.ReadAllText(@"C:\\MainStuff\\MT940 JSON.txt");
-            //BsonDocument document = BsonDocument.Parse(json);*/
-
-
-            string content = File.ReadAllText(@"C:\\MainStuff\\MT940 JSON.txt");
-
-            var data = new StringContent(content, Encoding.UTF8, "application/json");
-
-            var url = "http://127.0.0.1:122/api/upload";
-            using var client = new HttpClient();
-
-            var response = await client.PostAsync(url, data);
-
-            var result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
-
-
+            string fileToUpload = @"C:\\MainStuff\\MT940TXT.txt";
+            string url = "http://127.0.0.1:122/api/uploadFile";
+            using (var client = new WebClient())
+            {
+                byte[] result = client.UploadFile(url, fileToUpload);
+                string responseAsString = Encoding.Default.GetString(result);
+                Console.WriteLine(responseAsString);
+            }
         }
 
-        //FUNCTIONS NOT WORK YET
-        static async Task SendFilePOST(string document)
-        {
-            using var client = new HttpClient();
-            var data = new StringContent(document, Encoding.UTF8, "application/json");
-            var url = "http://127.0.0.1:122/api/upload";
-
-            var response = await client.PostAsync(url, data);
-
-            var result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
-
-        }
-
-        static async Task SendFileGET()
+        static async Task GetRequest()
         {
             using var client = new HttpClient();
             var content = await client.GetStringAsync("http://127.0.0.1:122/api/upload");
