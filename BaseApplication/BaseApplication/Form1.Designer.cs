5517421d76a19e68f51b940e67395eabc3256c74
Miroslav Penchev <miroslav.penchev@student.nhlstenden.com>
2023-02-16 02:59:29 +0100

    Added functionality to base app

--- BaseApplication/BaseApplication/Form1.Designer.cs
+++ BaseApplication/BaseApplication/Form1.Designer.cs
@@ -28,77 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.showTransac = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.showBalance = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.keyword = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // showTransac
             // 
-            this.button1.Location = new System.Drawing.Point(108, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.showTransac.Location = new System.Drawing.Point(12, 79);
+            this.showTransac.Name = "showTransac";
+            this.showTransac.Size = new System.Drawing.Size(147, 32);
+            this.showTransac.TabIndex = 0;
+            this.showTransac.Text = "Show Transactions";
+            this.showTransac.UseVisualStyleBackColor = true;
+            this.showTransac.Click += new System.EventHandler(this.showTransac_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(392, 84);
+            this.richTextBox1.Location = new System.Drawing.Point(392, 27);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(294, 301);
+            this.richTextBox1.Size = new System.Drawing.Size(387, 390);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
-            // button2
+            // showBalance
             // 
-            this.button2.Location = new System.Drawing.Point(108, 151);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.showBalance.Location = new System.Drawing.Point(12, 158);
+            this.showBalance.Name = "showBalance";
+            this.showBalance.Size = new System.Drawing.Size(147, 32);
+            this.showBalance.TabIndex = 2;
+            this.showBalance.Text = "Show Balance";
+            this.showBalance.UseVisualStyleBackColor = true;
+            this.showBalance.Click += new System.EventHandler(this.showBalance_Click);
             // 
-            // button3
+            // searchBtn
             // 
-            this.button3.Location = new System.Drawing.Point(108, 211);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.searchBtn.Location = new System.Drawing.Point(237, 158);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 3;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
-            // textBox1
+            // keyword
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 22);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 4;
+            this.keyword.AutoSize = true;
+            this.keyword.Location = new System.Drawing.Point(250, 65);
+            this.keyword.Name = "keyword";
+            this.keyword.Size = new System.Drawing.Size(62, 16);
+            this.keyword.TabIndex = 5;
+            this.keyword.Text = "Keyword:";
             // 
-            // label1
+            // comboBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Input:";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "available_balance",
+            "final_opening_balance",
+            "transactions",
+            "transaction_reference"});
+            this.comboBox1.Location = new System.Drawing.Point(201, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 43);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Add File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.keyword);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.showBalance);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.showTransac);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -108,12 +128,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button showTransac;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showBalance;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label keyword;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- BaseApplication/BaseApplication/Form1.cs
+++ BaseApplication/BaseApplication/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,18 +20,66 @@ namespace BaseApplication
             InitializeComponent();
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        async Task GetRequest(string request)
         {
-          await GetRequest();
-        }
-
-        async Task GetRequest()
-        {
+            string url;
+            if (request == "balance")
+            {
+                url = "http://127.0.0.1:122/api/getBalance/63eccee5211dcc98b44e182a";
+            }
+            else if(request == "" ){
+                url = "http://127.0.0.1:122/api/getTransactions";
+            }
+            else
+            {
+                url = "http://127.0.0.1:122/api/searchKeyword?transaction_id=63eccee5211dcc98b44e182a&element=" + request;
+            }
             using var client = new HttpClient();
-            var content = await client.GetStringAsync("http://127.0.0.1:122/api/getTransactions");
+            
+            var content = await client.GetStringAsync(url);
 
             richTextBox1.Text = content;
 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                richTextBox1.Text = file;
+                PostRequest(file);
+            }
+
+        }
+
+        void PostRequest(string fileToUpload)
+        {
+            string url = "http://127.0.0.1:122/api/uploadFile";
+            using (var client = new WebClient())
+            {
+                byte[] result = client.UploadFile(url, fileToUpload);
+                string responseAsString = Encoding.Default.GetString(result);
+                richTextBox1.Text = responseAsString;
+            }
+        }
+
+        private async void searchBtn_Click(object sender, EventArgs e)
+        {
+            string request = comboBox1.SelectedItem.ToString();
+            await GetRequest(request);
+        }
+
+        private async void showTransac_Click(object sender, EventArgs e)
+        {
+            await GetRequest("");
+        }
+
+        private async void showBalance_Click(object sender, EventArgs e)
+        {
+            await GetRequest("balance");
         }
     }
 }
