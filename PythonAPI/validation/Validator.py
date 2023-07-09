3b8623e6e0c82a363182180ad3ff34b2fa5a5ccd
KrystianWiazowskiV2 <89790188+bouncychair@users.noreply.github.com>
2023-07-09 19:29:37 +0200

    new validation for AddMember, Login, Register and Upload

--- PythonAPI/validation/Validator.py
+++ PythonAPI/validation/Validator.py
@@ -3,38 +3,42 @@ import jsonschema
 from lxml import etree
 import os
 
+# File paths
 current_directory = os.path.dirname(__file__)
-json_file_path = os.path.join(current_directory, 'JSON', 'validation.json')
-xml_file_path = os.path.join(current_directory, 'XML', 'XMLTest.xml')
-xsd_file_path = os.path.join(current_directory, 'XML', 'XSDSchemav3.xsd')
+json_file_path_upload = os.path.join(current_directory, 'JSON', 'upload_schema.json')
+json_file_path_modules = os.path.join(current_directory, 'JSON', 'modules_summary_schema.json')
+json_file_path_add_member = os.path.join(current_directory, 'JSON', 'add_member_schema.json')
+json_file_path_login = os.path.join(current_directory, 'JSON', 'login_schema.json')
+json_file_path_register = os.path.join(current_directory, 'JSON', 'register_schema.json')
+xsd_file_path_modules = os.path.join(current_directory, 'XML', 'modules_summary_schema.xsd')
+xsd_file_path_add_member = os.path.join(current_directory, 'XML', 'add_member_schema.xsd')
+xsd_file_path_login = os.path.join(current_directory, 'XML', 'login_schema.xsd')
+xsd_file_path_register = os.path.join(current_directory, 'XML', 'register_schema.xsd')
 
 # JSON Validation
-with open(json_file_path, 'r') as file:
-    schema = json.load(file)
+def json_schema_validate(json_file, schemapath):
+    with open(schemapath, 'r') as f:
+        schema = json.load(f)
 
-
-def json_schema_validate(json_file):
     try:
         jsonschema.validate(json_file, schema)
         return True
-    except jsonschema.exceptions.ValidationError:
+    except jsonschema.exceptions.ValidationError as e:
+        print(e)
         return False
 
+#XML Validation
+def xml_schema_validate(file, schemapath):
+    xml = etree.fromstring(file)
 
-# XML Validation
-with open(xml_file_path, 'r', encoding='utf-8') as xml_file:
-    xml_content = xml_file.read()
+    with open(schemapath, 'r') as f:
+        xsd = etree.parse(f)
 
-with open(xsd_file_path, 'r', encoding='utf-8') as xsd_file:
-    xsd_content = xsd_file.read()
-
-
-def xml_schema_validate(xml_file):
-    xmlschema = etree.XMLSchema(etree.fromstring(xsd_content))
-    parser = etree.XMLParser(schema=xmlschema)
-
+    schema = etree.XMLSchema(xsd)
     try:
-        etree.fromstring(xml_file.encode(), parser)
-        return True, None
+        schema.assertValid(xml)
+        return True
     except etree.XMLSyntaxError as e:
-        return False, str(e)
+        raise Exception('Malformed XML. %s' % e)
+    except etree.DocumentInvalid as e:
+        raise Exception('Invalid XML. %s' % e)
